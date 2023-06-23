2fd7645d75a2ba3b69c5609d33681addc2332f23
João Sereia <joao.lo.sereia@gmail.com>
2023-06-23 18:21:54 -0300

    add doc comment.

--- lib/libre_doc_converter.dart
+++ lib/libre_doc_converter.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// A library for converting LibreOffice documents to other formats.
 library;
 
 export 'src/libre_doc_converter_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
